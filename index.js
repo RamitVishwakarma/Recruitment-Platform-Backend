b3d467618e9183178b9e648fbae2a783e72aa1de
Piyush Gupta <guptapiyush315@gmail.com>
2024-01-10 18:26:30 +0530

    Add error details to response

--- index.js
+++ index.js
@@ -36,9 +36,13 @@ app.listen(port,(error)=>{
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
+    const errorDetails = process.env.NODE_ENV === 'development' ? err.stack : null;
+  
     return res.status(statusCode).json({
       success: false,
       statusCode,
       message,
+      error: errorDetails,
     });
   });
+  
