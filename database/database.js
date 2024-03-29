d5fbd4b787c5ff150734056fa5fe1946ba59a70b
Piyush Gupta <guptapiyush315@gmail.com>
2024-03-29 20:12:56 +0530

    db

--- database/database.js
+++ database/database.js
@@ -4,9 +4,11 @@ const dotenv=require("dotenv");
 dotenv.config();
 const connectdb=(DATABASE_URL)=>{
   
-    return mongoose.connect(DATABASE_URL,{
-        dbName: "Recruitment_Platform", 
-    }).then((success)=>{
+    return mongoose.connect(DATABASE_URL
+    // ,{
+    //     dbName: "Recruitment_Platform", 
+    // }
+    ).then((success)=>{
         console.log("database connected!");
 
     }).catch((error)=>{
