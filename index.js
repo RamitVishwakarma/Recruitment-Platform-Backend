0977671b4d625f4920847240cf5f4a026f4b2012
RamitVishwakarma <vishwakarmaramit@gmail.com>
2024-11-15 09:49:53 +0530

    Updated cors

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const fileUpload = require('express-fileupload')
-var morgan = require('morgan')
+const fileUpload = require("express-fileupload");
+var morgan = require("morgan");
 dotenv.config();
 
 const PORT = process.env.PORT || 80;
@@ -16,10 +15,9 @@ const connectdb = require("./database/database.js");
 connectdb(DATABASE_URL);
 // database end
 
-
 app.use("/public", express.static("Uploads"));
 app.use(express.json());
-app.use(morgan('tiny'));  //  morgan(':method :url :status :res[content-length] - :response-time ms')
+app.use(morgan("tiny")); //  morgan(':method :url :status :res[content-length] - :response-time ms')
 app.use(express.urlencoded({ extended: true }));
 // app.options('*', cors());
 // app.use(cors({exposedHeaders: ['Authorization']}));
@@ -34,49 +32,50 @@ app.use(express.urlencoded({ extended: true }));
 //     allowedHeaders: ['Content-Type', 'Authorization'],
 // }));
 
-
-const allowedOrigins = ['http://localhost:5173', 'https://gdsc-recruitment-2024.vercel.app'];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://gdsc-recruitment-2024.vercel.app",
+  "https://recplatform.ramitvishwakarma.in/",
+];
 
 // Configure CORS middleware
-app.use(cors({
+app.use(
+  cors({
     origin: function (origin, callback) {
-        // Check if the incoming origin is in the allowed origins list
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+      // Check if the incoming origin is in the allowed origins list
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     },
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
-
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // using routes
 const homeRoute = require("./routes/index.js");
 app.use("/", homeRoute);
 
 app.listen(PORT, (error) => {
-    if (error) {
-        console.log("server not started");
-    } else {
-        console.log(`server running at http://localhost:${PORT}`);
-
-
-    }
-})
-
+  if (error) {
+    console.log("server not started");
+  } else {
+    console.log(`server running at http://localhost:${PORT}`);
+  }
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    const errorDetails = process.env.NODE_ENV === 'development' ? err.stack : null;
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  const errorDetails =
+    process.env.NODE_ENV === "development" ? err.stack : null;
 
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-        error: errorDetails,
-    });
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+    error: errorDetails,
+  });
 });
